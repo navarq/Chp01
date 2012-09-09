6d6ce69b0ddaac23329c500e676a5edcba156e44
Basil Tungekar <basil.tungekar@live.com>
2012-09-09 11:33:53 +0100

    first commit

---
+++ Framework/CalcCore/Math/Finance.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Finance.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace CalcCore.Math
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Finance
+    {
+    }
+}
